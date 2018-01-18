e06ecbe0d3a86fa51ba74139be20bec2134c311d
akgoode <akgoode@gmail.com>
2018-01-17 20:39:18 -0600

    css is bundled properly

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
+require('../stylesheets/index.css');
 
 var $container = $('.container');
 
 let hi = "Kenzie";
+
+$container.append('<p>' + hi + ' Im javascript</p>');
 
 $container.append('<p>' + hi + ' Im javascript</p>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
         application: './scripts/index.js',
-        vendor: 'jquery'
+        vendor: 'jquery',
+        styles: './stylesheets/index.css'
     },    
     output: {
         filename: '[name].js',
@@ -12,17 +14,6 @@ module.exports = {
         publicPath: 'dist/'
     },
     watch: true,
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: Infinity
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        })
-    ],
     module: {
         loaders: [
             {
@@ -32,7 +23,23 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.css/,
+                loader: ExtractTextPlugin.extract('css-loader'),
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin("styles.css")
+    ],
 };
